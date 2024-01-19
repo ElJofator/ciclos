565eabab829e39bc2d7a888bb3b3e499f372eecd
79776204+ElJofator@users.noreply.github.com <ElJofator>
2024-01-19 13:24:57 -0500

    Renombrar bandera a pedir

--- tiempo_de_viaje.py
+++ tiempo_de_viaje.py
@@ -1,12 +1,12 @@
 # Un viajero desea saber cuánto tiempo tomó un viaje que realizó. Él tiene la duración en minutos de cada uno de los tramos del viaje.
 # Desarrolle un programa que permita ingresar los tiempos de viaje de los tramos y entregue como resultado el tiempo total de viaje en formato horas:minutos.
 # El programa deja de pedir tiempos de viaje cuando se ingresa un 0.
-bandera = True
+pedir = True
 minutos = 0
-while(bandera):
+while(pedir):
     tramo = int(input("Duración tramo: "))
     if(tramo == 0):
-        bandera = False
+        pedir = False
     else:
         minutos += tramo
 horas = 0
